440d7d90f2fefb29452aabd9de4fb99d6ba9f9f1
giuliamasoq <117548702+giuliamasoq@users.noreply.github.com>
2022-11-06 12:03:33 -0300

    .

--- src/hack1000DevsA6/Hackathon.java
+++ src/hack1000DevsA6/Hackathon.java
@@ -8,13 +8,21 @@ import java.net.URI;
 public class Hackathon {      
 
         public static void main (String [] args) {
+
+                String link = "https://programathor.com.br/jobs";
+                boolean naoTemLink = false;
+                while (naoTemLink==false) {
+                        link = acessarPagina(link);
+                        naoTemLink = link.equals("");
+                }
+        
+        }
+
+        public static String acessarPagina (String link) {
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request;
 
                 HttpResponse <String> response = null;
-
-                String link = "https://programathor.com.br/jobs";
-                
                 request = HttpRequest.newBuilder().uri(URI.create(link)).GET().build();
 
                 try {
@@ -29,7 +37,7 @@ public class Hackathon {
 
                 /* Para reduzir a string de HTML */
 				
-                String idTitulo = "<h3 class=\"text-24 line-height-30\">";
+                String idTitulo = "<div class=\"col-md-9\">";
 				int posicaoInicial = html.indexOf(idTitulo);
 				html = html.substring(posicaoInicial);
 
@@ -38,17 +46,34 @@ public class Hackathon {
                 html=html.replaceAll(labelNew,"");
 
 		posicaoInicial = 0;
-                while (posicaoInicial >-1) {
+                while (posicaoInicial !=-1) {
                 	posicaoInicial = pegarInformacoes (html, posicaoInicial);
+                        if(posicaoInicial!=-1){
                 	html = html.substring(posicaoInicial);
+                        }
+                }
+                boolean temVencida = false;
+                while (temVencida == false) {              
+                        
+                        return link;
                 }
         }
 
+
         public static int pegarInformacoes (String html, int posicaoInicial) {
         	
+                String idLinkVaga = "<a href=\"";
+                String idFimLinkVaga = "<div class=\"row\">";
+                posicaoInicial = html.indexOf(idLinkVaga);
+                int posicaoFinal = html.indexOf(idFimLinkVaga);
+                posicaoInicial += idLinkVaga.length();     
+                String link = (html.substring(posicaoInicial,(posicaoFinal)));
+                idFimLinkVaga = "\">";
+                posicaoFinal = link.indexOf(idFimLinkVaga);
+                link = link.substring(0, posicaoFinal);   
+
         	String idTitulo = "<h3 class=\"text-24 line-height-30\">";
         	String idFimTitulo = "</h3>";
-        	int posicaoFinal;
         	posicaoInicial = html.indexOf(idTitulo);
         	posicaoFinal = html.indexOf(idFimTitulo);
         	posicaoInicial += idTitulo.length();
@@ -61,9 +86,42 @@ public class Hackathon {
         	posicaoInicial += idEmpresa.length();
           	System.out.println("Empresa: " + (html.substring(posicaoInicial,posicaoFinal)));
 
-                posicaoFinal += idFimEmpresa.length();
-          	
+                String idCidade = "<i class='fas fa-map-marker-alt'></i>";
+                String idFimCidade = "</span><span><i class='fa fa-building'>";
+                posicaoInicial = html.indexOf(idCidade);
+        	posicaoFinal = html.indexOf(idFimCidade);
+        	posicaoInicial += idCidade.length();
+                String cidade = html.substring(posicaoInicial,posicaoFinal);
+                if (cidade.equals("Remoto")) {
+                        System.out.println("Cidade: Não se aplica");
+                        System.out.println("Tipo de Vaga: Remoto");
+                } else {
+                        System.out.println("Cidade: " + cidade);
+                        System.out.println("Tipo de Vaga: Presencial");
+                }
+                
+                String idSalario = "<i class='far fa-money-bill-alt'></i>";
+                String idFimSalario = "</span><span><i class='far fa-chart-bar'>";
+                posicaoInicial = html.indexOf(idSalario);
+                posicaoFinal = html.indexOf(idFimSalario);
+                posicaoInicial += idSalario.length();
+                if (posicaoInicial>posicaoFinal) {
+                        System.out.println("Salario: Não informado");
+                } else {
+                        System.out.println("Salario: " + (html.substring(posicaoInicial,posicaoFinal)));
+                }
+
+                System.out.println("Link: https://programathor.com.br" + link);
+
+                posicaoFinal += idFimSalario.length();
+          	System.out.println();
+                String existeMaisVagaNaPagina = html.substring(posicaoFinal);
+                int testeMaisVaga = existeMaisVagaNaPagina.indexOf(idTitulo);
+                if (testeMaisVaga != -1) {
 			return posicaoFinal;
+                } else {
+                        return -1;
+                }
         }
 
 }
